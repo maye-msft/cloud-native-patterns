287f4302c2a61c404b813aedb6f4dc28a7e28b10
maye-msft <clicpilot@gmail.com>
2022-06-25 22:29:39 +0800

    fix: git dest folder

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -14,9 +14,8 @@ async function execa(cmd, options) {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     console.log("Building...");
     await execa("npm", ["run", "docs:build"]);
-    await execa("cd", ["docs/.vuepress"]);
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    const folderName = "docs/.vuepress/dist"
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
